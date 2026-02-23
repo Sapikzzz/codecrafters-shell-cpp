5df6ea61f82a1b3b5d8b605fa3fbf9a0f33986a5
sapikzzz <2002jakubs@gmail.com>
2026-02-24 00:51:40 +0100

    codecrafters submit [skip ci]

---
+++ src/commands.h
@@ -0,0 +1,6 @@
+#pragma once
+
+enum class Commands {
+	EXIT,
+	CD,
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  std::string user_input = "";
-
-  while(1){
-	  std::cout << "$ ";
-	  std::cin >> user_input;
-	  std::cout << user_input << ": command not found\n";
-  }
+	std::string user_input = "";
+  	while(1){
+		std::cout << "$ ";
+		std::cin >> user_input;
+		if(user_input == "exit"){
+			return 0;
+		}
+		std::cout << user_input << ": command not found\n";
+   }
 }

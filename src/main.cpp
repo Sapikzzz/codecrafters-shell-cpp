095e25af6c7a702f00c3be9c5020b51e5bd32f71
sapikzzz <2002jakubs@gmail.com>
2026-02-24 00:36:21 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  std::string user_input = "";
+
   std::cout << "$ ";
+  std::cin >> user_input;
+  std::cout << user_input << ": command not found";
 }

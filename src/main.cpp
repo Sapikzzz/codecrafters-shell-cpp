03cd6f789918bf0a34ee3603ed78adb219399c8a
sapikzzz <2002jakubs@gmail.com>
2026-02-24 01:22:00 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,20 @@ int main() {
 		if(user_input == "exit"){
 			return 0;
 		}
-		else if(user_input.substr(0, 4) == "echo"){
+		else if(user_input.substr(0, user_input.find(" ")) == "echo"){
 			std::cout << user_input.substr(5) << "\n";
+		}
+		else if(user_input.substr(0, user_input.find(" ")) == "type"){
+			std::string target_command = user_input.substr(user_input.find(" ") + 1, user_input.find("\n"));
+			if(target_command == "echo" ){
+				std::cout << target_command << "is a shell builtin\n";
+			}
+			else if(target_command == "exit"){
+				std::cout << target_command << "is a shell builtin\n";
+			}
+			else{
+				std::cout << target_command << ": not found";
+			}
 		}
 		else{
 			std::cout << user_input << ": command not found\n";
